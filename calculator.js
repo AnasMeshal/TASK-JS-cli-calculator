8fcd5ddd5f45a8c23ec90e075ef56b9bcc4f8a54
AnasMarzouq <63076945+AnasMeshal@users.noreply.github.com>
2024-10-15 14:49:31 +0300

    my two workin cells couldn't do better than this

--- calculator.js
+++ calculator.js
@@ -1,6 +1,55 @@
 const prompt = require("prompt-sync")(); // Please don't touch me :)
 
-// The prompt method allows us to take an input from the user.
-// Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+// // taking the first number
+let num1 = prompt("Please enter the first number:");
+let num2;
+let operation;
+let result;
+
+if (isNaN(num1) || num1 == "") {
+  console.log("Oops! Please enter a valid number");
+  return;
+} else {
+  num1 = parseFloat(num1);
+  num2 = prompt("Please enter the second number:");
+  if (isNaN(num2)) {
+    console.log("Oops! Please enter a valid number");
+    return;
+  } else {
+    num2 = parseFloat(num2);
+    operation = prompt("Please choose the operation (+, -, /, *):");
+    if (
+      operation !== "+" &&
+      operation !== "-" &&
+      operation !== "/" &&
+      operation !== "*"
+    ) {
+      console.log("Oops! Please enter a valid operation method");
+    } else {
+      switch (operation) {
+        case "+":
+          result = Number(num1) + Number(num2);
+          break;
+
+        case "-":
+          result = num1 - num2;
+          break;
+
+        case "/":
+          result = Number(num1) / Number(num2);
+          break;
+        case "*":
+          result = Number(num1) * Number(num2);
+          break;
+
+        default:
+          break;
+      }
+      console.log(`${num1} ${operation} ${num2} = ${result}`);
+    }
+  }
+}
+
+// const num1 = isNaN(prompt("whats your first number?"));
+
+// console.log(typeof num1, num1);
